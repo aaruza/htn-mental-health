01826f1b361e62c85be22aef7b011b61fedefd4f
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2021-01-16 15:38:33 -0500

    finished get/post for journal entries

--- root/backend/app.py
+++ root/backend/app.py
@@ -1,16 +1,46 @@
-from flask import Flask
+from flask import Flask, request, flash
 import sqlalchemy
 
-app = Flask(__name__)
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy.orm
+from cockroachdb.sqlalchemy import run_transaction
 
-engine = sqlalchemy.create_engine('postgres://aaron:lxS7zu03CFHvhExE@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/serene-mule-276.defaultdb?sslmode=verify-full&sslrootcert=/cc-ca.crt')
+
+app = Flask(__name__)
+app.config.from_pyfile('app.cfg')
+db = SQLAlchemy(app)
+sessionmaker = sqlalchemy.orm.sessionmaker(db.engine)
+
+
+class JournalEntry(db.Model):
+    __tablename__ = 'journal'
+    id = db.Column('id', db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    text = db.Column(db.String)
+    date = db.Column(db.DateTime)
+
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+        self.date = datetime.utcnow()
 
 @app.route('/api/journal', methods=['POST', 'GET'])
 def journal_entries():
-    if request.method == 'POST':
-        pass
+    if request.method == 'GET':
+        values = []
+        def callback(session):
+            values =  (session.query(JournalEntry).all())
+        run_transaction(sessionmaker, callback)
+        flash('hi')
+        return str(values)
     else:
-        return {
+        def callback(session):
+            entry = JournalEntry(request.form['title'], request.form['text'])
+            session.add(entry)
+        run_transaction(sessionmaker, callback)
 
-        }
+        return str(request.form)
 
+if __name__ == '__main__':
+    app.run()
