fdfabf8e4d79e4d12a3494e168b08af42d4fe979
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2021-01-16 12:50:07 -0500

    added certificate

--- root/backend/app.py
+++ root/backend/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+import sqlalchemy
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+engine = sqlalchemy.create_engine('postgres://aaron:lxS7zu03CFHvhExE@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/serene-mule-276.defaultdb?sslmode=verify-full&sslrootcert=/cc-ca.crt')
+
+@app.route('/api/journal', methods=['POST', 'GET'])
+def journal_entries():
+    if request.method == 'POST':
+        pass
+    else:
+        return {
+
+        }
+
