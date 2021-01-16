527b2fd66e245bc20ca2e0f7aac8ffb1ba5e770b
Picowchew <58180935+Picowchew@users.noreply.github.com>
2021-01-16 13:49:42 -0500

    Added Journal page

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Mainpage from "./mainpage/mainpage";
+import Journal from "./journal/journal";
 import Errorpage from "./errorpage/errorpage";
 
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route path="/" component={Mainpage} exact />
-        <Route component={Errorpage} />
-      </Switch>
-    </div>
+    <Switch>
+      <Route path="/" component={Mainpage} exact />
+      <Route path="/journal" component={Journal} />
+      <Route component={Errorpage} />
+    </Switch>
   );
 }
 
